d63afeaaa3e9a3997c2b1344052fce2a7728db9b
Komefumi <komefumi@protonmail.ch>
2021-12-22 19:10:19 +0530

    move to a gulp based workflow for styles

---
+++ Gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const rimraf = require("rimraf");
+
+const { series, watch } = gulp;
+
+function cleanStylesOutput() {
+  rimraf.sync("./app/styles/compiled/*");
+}
+
+function buildStyles() {
+  const plugins = [autoprefixer({}), cssnano()];
+  return gulp
+    .src("./styles/entry/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest("./app/styles/compiled"));
+}
+
+
+exports.cleanStylesOutput = cleanStylesOutput;
+exports.buildStyles = buildStyles;
+
+exports.compileStylesForProduction = function compileStylesForProduction() {
+  cleanStylesOutput();
+  buildStyles();
+}
+
+exports.watchAndCompileStyles = function watchAndCompileStyles() {
+  cleanStylesOutput();
+  buildStyles();
+  watch("./styles/entry/*.scss", { queue: false }, function () {
+    cleanStylesOutput();
+    buildStyles();
+  });
+};
